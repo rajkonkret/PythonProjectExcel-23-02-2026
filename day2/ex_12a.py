5996e92770469f3902c97843d1b3c854cc73cac8
rajkonkret <rajkonkret660@gmail.com>
2026-02-24 12:18:31 +0100

    [2 18] update ex12a.py

--- day2/ex_12a.py
+++ day2/ex_12a.py
@@ -13,5 +13,7 @@ df = pd.DataFrame(data, columns=column_names)
 
 writer = pd.ExcelWriter("excel_with_list.xlsx", engine="xlsxwriter")
 
-df.to_excel(writer)
+# df.to_excel(writer)
+# df.to_excel(writer, index=False)
+df.to_excel(writer, index=False, sheet_name='first_sheet', startrow=3, startcol=3)
 writer.close()
