c15f9ad2837421e7e03a8e8c4204feb4d9eccb47
rajkonkret <rajkonkret660@gmail.com>
2026-02-24 12:49:03 +0100

    [2 22] update ex16.py

--- day2/ex16.py
+++ day2/ex16.py
@@ -6,3 +6,17 @@ height_data = [
     {"Name": "Darek", "Height": 199},
     {"Name": "John", "Height": 169},
 ]
+
+weight_data = [
+    {"Name": "Addiya", "Weight": 79},
+    {"Name": "Samen", "Weight": 89},
+    {"Name": "Darek", "Weight": 99},
+    {"Name": "John", "Weight": 69},
+]
+
+marks_data = [
+    {"Name": "Addiya", "Marks": 179},
+    {"Name": "Samen", "Marks": 189},
+    {"Name": "Darek", "Marks": 199},
+    {"Name": "John", "Marks": 169},
+]
