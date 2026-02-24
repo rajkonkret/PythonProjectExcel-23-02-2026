b2177140a822f67afc1a9f844c315b3d487765af
rajkonkret <rajkonkret660@gmail.com>
2026-02-24 09:00:47 +0100

    [2 3] update ex6.py

---
+++ day2/ex6.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+filename = 'video2.xlsx'
+
+# wb = openpyxl.load_workbook('video2.xlsx')
+wb = openpyxl.load_workbook(filename)
+ws = wb['vgsales']
+
+ws['P1'] = "Averages Sales"
+ws['P2'] = '=AVERAGE(K2:K16328)'
+
+wb.save(filename)
+wb.close()
