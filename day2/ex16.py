29e2b8a4e0d3a9725ccec2f38e231f1e0c82000d
rajkonkret <rajkonkret660@gmail.com>
2026-02-24 12:47:56 +0100

    [2 21] update ex16.py

---
+++ day2/ex16.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+height_data = [
+    {"Name": "Addiya", "Height": 179},
+    {"Name": "Samen", "Height": 189},
+    {"Name": "Darek", "Height": 199},
+    {"Name": "John", "Height": 169},
+]
